22d9091f5b69dd1d0f7256d77cb26112438ab4b8
MarcoDolya <72494263+MarcoDolya@users.noreply.github.com>
2020-10-16 14:12:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -27,14 +27,14 @@ $user_name = "Marco_Dolya"; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu"> 
-            <?php if ($is_auth == 1) {
+            <?php if ($is_auth == 1) 
             <div class="user-menu__logged">
               <p>Marco_Dolya</p>
                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                <a class="user-menu__logout" href="#">Выход</a>
             </div>   # code...
-        }
-            elseif ($is_auth == 0) {
+        
+            elseif ($is_auth == 0) 
                 <ul class="user-menu__list">
                   <li class="user-menu__item">
                   <a href="#">Регистрация</a>
@@ -43,7 +43,7 @@ $user_name = "Marco_Dolya"; // укажите здесь ваше имя
                   <a href="#">Вход</a>
                   </li>
                 </ul>
-            };
+            
         
 
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
